a95c80cd5146552879c2939ceefe79124c6c75ab
JRAAXX <140764985+JRAAXX@users.noreply.github.com>
2023-07-30 20:42:01 -0400

    have a lovely

--- src/team6/DemoTeam6.java
+++ src/team6/DemoTeam6.java
@@ -3,6 +3,8 @@ package team6;
 public class DemoTeam6 {
 	public static void main(String[] args) {
 		System.out.println("Team 8");
+		System.out.println("Jessica");
+		System.out.println ("Team006");
 	}
 
 }
